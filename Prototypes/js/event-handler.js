699de4591dcceb488619797da746507fbc38ea69
Monica Ramírez <monicaramirez@Monicas-iMac.local>
2015-09-02 10:10:17 -0500

    Added back-to-top icon and to porsche icon max-width: 800px

--- Prototypes/js/event-handler.js
+++ Prototypes/js/event-handler.js
@@ -6,18 +6,24 @@ $(document).ready(function(){
         e.preventDefault();
         $('#main-container').css('z-index', 0);
         $('#whats-hot-container').css('z-index', 1000).show();
-/*
-        var xAxis = e.pageX;
-        var yAxis = e.pageY;
-
-        $('#exit-link').css('left', xAxis).css('top', yAxis);
-*/
     });
 
     $('.exit-link').click(function(e){
         e.preventDefault();
         $('#main-container').css('z-index', 1000);
         $('#whats-hot-container').css('z-index', 0).hide();
+    });
+
+    // ===== Scroll to Top ====
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
+            $('.back-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('.back-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+
+    $('.back-to-top-img').click(function(){
 
     });
 
