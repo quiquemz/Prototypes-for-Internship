dc146a9f13947fc74ade9f574eec219e53a65fa3
Monica Ramírez <monicaramirez@Monicas-iMac.local>
2015-09-03 14:53:04 -0500

    Fixed important details (take-overs)

--- Prototypes/js/event-handler.js
+++ Prototypes/js/event-handler.js
@@ -1,19 +1,5 @@
 $(document).ready(function(){
 
-    $('#whats-hot-container').hide();
-
-    $('#hot-link').click(function(e){
-        e.preventDefault();
-        $('#main-container').css('z-index', 0);
-        $('#whats-hot-container').css('z-index', 1000).show();
-    });
-
-    $('.exit-link').click(function(e){
-        e.preventDefault();
-        $('#main-container').css('z-index', 1000);
-        $('#whats-hot-container').css('z-index', 0).hide();
-    });
-
     // ===== Scroll to Top ====
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
@@ -23,8 +9,51 @@ $(document).ready(function(){
         }
     });
 
-    $('.back-to-top-img').click(function(){
 
+
+    // ==== Hiding all take-overs ====
+    $('.take-over').hide();
+
+
+    // ==== Showing and Hiding SEARCH ====
+    $('#search-link').click(function(e){
+        e.preventDefault();
+        $('#main-container').css('z-index', 0);
+        $('#search-take-over').css('z-index', 1000).show();
+
+        $('.exit-link').css('left', e.pageX - 15).css('top', e.pageY - 15);
     });
 
+    $('.exit-link').click(function(e){
+        e.preventDefault();
+        $('#main-container').css('z-index', 1000);
+        $('#search-take-over').css('z-index', 0).hide();
+    });
+
+
+    // ==== Changing LANGUAGE ====
+    $('#lang-link').click(function(e){
+        e.preventDefault();
+        if ($('#lang-link').html() === 'EN') {
+            $('#lang-link').html("ES");
+        }
+        else {
+            $('#lang-link').html("EN");
+        }
+    });
+
+    // ==== Showing and Hiding MENU ====
+    $('#menu-link').click(function(e){
+        e.preventDefault();
+        $('#main-container').css('z-index', 0);
+        $('#menu-take-over').css('z-index', 1000).show();
+
+        $('.exit-link').css('left', e.pageX - 15).css('top', e.pageY - 15);
+    });
+
+    $('.exit-link').click(function(e){
+        e.preventDefault();
+        $('#main-container').css('z-index', 1000);
+        $('#menu-take-over').css('z-index', 0).hide();
+    });
 });
